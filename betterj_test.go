20a36353d526c29d8c06463d8a9ba20e220a0343
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2024-12-30 13:09:36 +0500

    add the test file

--- betterj_test.go
+++ betterj_test.go
@@ -4,46 +4,41 @@ import (
 	"testing"
 )
 
-//We defined the input and output format for the function we created
-//as mentioned here: [Creating go module](https://dev.to/kingkunte_/go-modules-beginners-guide-4a7p#:~:text=In%20the%20Go%20programming%20language,in%20the%20module's%20root%20directory.)
-
+// Defined the input and output format for the function we created
 func TestMinifyJ(t *testing.T) {
 	input := `{
-		"name": "John",
-		"age": 30,
-		"city": "New York"
+		"name": "Hassan Jawwad",
+		"age": 22,
+		"city": "Lahore"
 	}`
-	want := `{"name":"John","age":30,"city":"New York"}`
+	want := `{"name":"Hassan Jawwad","age":22,"city":"Lahore"}`
 
 	t.Run(input, func(t *testing.T) {
 		got, err := MinifyJ(input)
 		if err != nil {
-			t.Errorf("MinifyJ failed: %v", err)
+			panic(err)
 		}
 		if got != want {
 			t.Errorf("expected %v, got %v", want, got)
 		}
 	})
-
 }
-
 func TestBeautifyJ(t *testing.T) {
-	input := `{"name":"John","age":30,"city":"New York"}`
+	input := `{"name":"Hassan Jawwad","age":22,"city":"Lahore"}`
 	indent := "  "
 	want := `{
-  "name": "John",
-  "age": 30,
-  "city": "New York"
+  "name": "Hassan Jawwad",
+  "age": 22,
+  "city": "Lahore"
 }`
 
 	t.Run(input, func(t *testing.T) {
 		got, err := BeautifyJ(input, indent)
 		if err != nil {
-			t.Errorf("BeautifyJ failed: %v", err)
+			panic(err)
 		}
 		if got != want {
 			t.Errorf("expected %v, got %v", want, got)
 		}
 	})
-
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hassanjawwad12/betterj
+
+go 1.23.4
