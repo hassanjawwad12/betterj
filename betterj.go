e9bf6b7a042f431fd62b8d7c5a8ce9408cbbc351
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2024-12-31 10:59:17 +0500

    made the string to byte conversion explicit

--- betterj.go
+++ betterj.go
@@ -5,12 +5,16 @@ import (
 	"encoding/json"
 )
 
+//I used the bytes.buffer because its dynamic and works good with compact and indent
+
 func MinifyJ(j string) (string, error) {
-	//I used the bytes.buffer because its dynamic and works good with compact and indent
 	var buffer bytes.Buffer
 
+	//json string converted to bytes for the compact function
+	bytesData := []byte(j)
+
 	//json.Compact will remove the whitespace and the new lines
-	err := json.Compact(&buffer, []byte(j))
+	err := json.Compact(&buffer, bytesData)
 	if err != nil {
 		return "", err
 	}
@@ -18,11 +22,13 @@ func MinifyJ(j string) (string, error) {
 }
 
 func BeautifyJ(j string, indentWith string) (string, error) {
-
 	var buffer bytes.Buffer
 
+	bytesData := []byte(j)
+
 	//json.Indent will format the JSON with custom indentation
-	if err := json.Indent(&buffer, []byte(j), "", indentWith); err != nil {
+	err := json.Indent(&buffer, bytesData, "", indentWith)
+	if err != nil {
 		return "", err
 	}
 	return buffer.String(), nil
